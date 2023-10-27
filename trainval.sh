65f879429a525943c8d95520ed993ce388680628
Niccolò Petti <niccolo.petti@gmail.com>
2023-10-27 23:27:59 +0200

    updated trainval.sh

--- trainval.sh
+++ trainval.sh
@@ -4,8 +4,8 @@ python main_dnd.py \
  --use_cache \
  --cfg ./configs/WW2020.yml \
  --exp_name WW2020_swinv2s \
- --im_scale 256 \
- --bs 16 \
- --acc_bsz 2 \
+ --im_scale 896 \
+ --bs 2 \
+ --acc_bsz 1 \
  --codalab_pred test \
  --train_set train
